5dddc43fc86448b61fd66f00baa3f3657578d826
João Lucas <joaolucasfp2001@gmail.com>
2021-02-19 15:41:54 -0300

    I separated some if and else blocks in separate APIClient methods

--- Breeds/Network/APIClient/APIClient.swift
+++ Breeds/Network/APIClient/APIClient.swift
@@ -20,25 +20,12 @@ extension APIClient {
         let task = self.session.dataTask(with: request) { data, response, error in
 
             guard let httpResponse = response as? HTTPURLResponse else {
-                if let error = error as NSError?, error.domain == NSURLErrorDomain && error.code == NSURLErrorCancelled {
-                    completion(nil, .requestCancelled)
-                } else {
-                    completion(nil, .requestFailed(reason: error?.localizedDescription))
-                }
+                self.errorHandling(response: response, error: error, completion: completion)
                 return
             }
             switch httpResponse.statusCode {
             case 200...399:
-                if let data = data {
-                    do {
-                        let model = try JSONDecoder().decode(decodingType, from: data)
-                        completion(model, nil)
-                    } catch {
-                        completion(nil, .jsonConversionFailure)
-                    }
-                } else {
-                    completion(nil, .invalidData)
-                }
+                self.validationData(data: data, decodingType: decodingType, completion: completion)
             default:
                 completion(nil, .responseUnsuccessful)
             }
@@ -65,6 +52,31 @@ extension APIClient {
             }
         }
         task.resume()
+    }
+    
+    private func errorHandling<T: Decodable>(response: URLResponse?, error: Error?, completion: @escaping (T?, APIError?) -> Void) {
+        if let error = error as NSError?, error.domain == NSURLErrorDomain && error.code == NSURLErrorCancelled {
+            completion(nil, .requestCancelled)
+        } else {
+            completion(nil, .requestFailed(reason: error?.localizedDescription))
+        }
+    }
+    
+    private func validationData<T: Decodable>(data: Data?, decodingType: T.Type, completion: @escaping (T?, APIError?) -> Void) {
+        if let data = data {
+            jsonDecoder(completion: completion, decodingType: decodingType, data: data)
+        } else {
+            completion(nil, .invalidData)
+        }
+    }
+    
+    private func jsonDecoder<T: Decodable>(completion: @escaping (T?, APIError?) -> Void, decodingType: T.Type, data: Data) {
+        do {
+            let model = try JSONDecoder().decode(decodingType, from: data)
+            completion(model, nil)
+        } catch {
+            completion(nil, .jsonConversionFailure)
+        }
     }
 
     func cancelRequests() {
